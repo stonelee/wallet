6ee341c40b24d8b47a826b36f86fa37ed9a684d8
stonelee <istonelee@gmail.com>
2013-12-03 09:24:03 +0800

    kjMoney

--- js/directives.js
+++ js/directives.js
@@ -1,20 +1,18 @@
 (function() {
   'use strict';
 
-  angular.module('walletDirectives', []).directive('money', function() {
+  angular.module('walletDirectives', []).directive('kjMoney', function() {
     return {
       restrict: 'EA',
-      scope: {
-        number: '@'
+      scope: true,
+      link: function(scope, element, attrs, a) {
+        scope.$watch(attrs.kjMoney, function(number) {
+          scope.way = number < 0 ? '借帐' : '帮付';
+          scope.warn = number < 0;
+          scope.money = '¥' + Math.abs(number);
+        });
       },
-      link: function(scope, element, attrs,a) {
-        var number = scope.number;
-        scope.way = number < 0 ? '借帐' : '帮付';
-        scope.warn = number < 0;
-        scope.money = '¥' + Math.abs(number);
-      },
-      template: '<span class="money"><span ng-class="{warn:warn}">[{{way}}]</span>{{money}}</span>',
-      replace: true
+      template: '<span ng-class="{warn:warn}">[{{way}}]</span>{{money}}'
     };
   });
 
